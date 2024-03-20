2fca76a37d2e059ac6bfbf04b84d085e3999d656
Tecnologiazc2Roger <tecnologia2@zcmayoristas.com>
2024-03-20 09:18:30 -0500

    actualizar

---
+++ resources/views/pages/pedido/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.zc_app')
+@section('title', 'Pago Digital')
+@section('content')
+
+
+<section>
+    <div class="container">
+        <div class="row flex-center min-vh-100 py-5">
+        <div class="container">
+    <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+        </div>
+    </div>
+</section>
+@endsection
+@push('script-app')
+<script src="{{asset('assets/js/helper.js')}}"></script>
+<script>
+    
+</script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.login.index');
 });
+
+Route::get('/pedido', function () {
+    return view('pages.pedido.index');
+});
