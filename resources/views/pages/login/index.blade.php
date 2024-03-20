89b51e98151523fdeb48151c73fe3c4034417507
Tecnologiazc2Roger <tecnologia2@zcmayoristas.com>
2024-03-20 17:23:49 -0500

    creacion de modales para la pagina pedido

--- resources/views/pages/login/index.blade.php
+++ resources/views/pages/login/index.blade.php
@@ -4,16 +4,17 @@
 
 <!-- Importar el componente Modal -->
 @component('components.organisms.modal_mesaje', [
-  'modalId' => 'mensajeModal', // ID único para la modal
-  'modalSize' => 'modal-sm',    // Tamaño de la modal
-  'modalHeaderJustify' => 'justify-content-center',
-  'modalTitle' => 'Título de la Modal 1', // Título de la modal
-  'showCloseButton' => false, // Mostrar el botón de cierre
-  'textAlign' => 'text-center',    // Justificar el texto
-  'modalFooter' => '<button type="button" class="btn btn-lg btn-dark-zc fw-bold w-100" data-bs-dismiss="modal">Aceptar</button>'
+'modalId' => 'mensajeModal', // ID único para la modal
+'modalSize' => 'modal-sm', // Tamaño de la modal
+'modalHeaderJustify' => 'justify-content-center',
+'modalTitle' => 'Título de la Modal 1', // Título de la modal
+'showCloseButton' => false, // Mostrar el botón de cierre
+'textAlign' => 'text-center', // Justificar el texto
+'modalFooter' => '<button type="button" class="btn btn-lg btn-dark-zc fw-bold w-100"
+    data-bs-dismiss="modal">Aceptar</button>'
 ])
-  <!-- Contenido de la modal -->
-  <p>Contenido de la Modal 1 aquí...</p>
+<!-- Contenido de la modal -->
+<p>Contenido de la Modal 1 aquí...</p>
 @endcomponent
 <section>
     <div class="container">
@@ -28,13 +29,17 @@
                     <div class="card-body">
                         <div class="text-center mb-5">
                             <h5 class="fw-bold text-body-highlight">¿Listo para ver tus pedidos?</h5>
-                            <p class="text-body-tertiary">Por favor, introduce tu número de identificación para acceder a tus pedidos</p>
+                            <p class="text-body-tertiary">Por favor, introduce tu número de identificación para acceder
+                                a tus pedidos</p>
                         </div>
                         <form class="row g-3 needs-validation" novalidate action="#" method="post" id="rucForm">
                             @csrf
                             <div class="col-md-12">
-                                <label for="numeroIdentificacion" class="form-label fs-sm d-none">Número de idetificación</label>
-                                <input type="number" class="form-control form-control-lg" name="numeroIdentificacion" id="numeroIdentificacion" placeholder="Ingresa tu número de identificación (ruc)" required />
+                                <label for="numeroIdentificacion" class="form-label fs-sm d-none">Número de
+                                    idetificación</label>
+                                <input type="number" class="form-control form-control-lg" name="numeroIdentificacion"
+                                    id="numeroIdentificacion" placeholder="Ingresa tu número de identificación (ruc)"
+                                    required />
                                 <div class="invalid-feedback">
                                     Número de identificación incorrecto. Por favor, inténtalo de nuevo.
                                 </div>
@@ -44,7 +49,8 @@
                             </div>
                             <div class="col-md-12 d-none">
                                 <div class="fs-xs">
-                                    Al continuar, aceptas las <a href="#">Condiciones de uso</a> y el <a href="#">Aviso de privacidad</a> de ZC Mayoristas
+                                    Al continuar, aceptas las <a href="#">Condiciones de uso</a> y el <a href="#">Aviso
+                                        de privacidad</a> de ZC Mayoristas
                                 </div>
                             </div>
                         </form>
@@ -69,52 +75,51 @@
 @push('script-app')
 <script src="{{asset('assets/js/helper.js')}}"></script>
 <script>
-    // Función para mostrar el modal con el mensaje
-    function mostrarMensajeModal(titulo, mensaje) {
-        let modalTitle = document.querySelector('#mensajeModal .modal-title');
-        let modalBody = document.querySelector('#mensajeModal .modal-body');
+// Función para mostrar el modal con el mensaje
+function mostrarMensajeModal(titulo, mensaje) {
+    let modalTitle = document.querySelector('#mensajeModal .modal-title');
+    let modalBody = document.querySelector('#mensajeModal .modal-body');
 
-        modalTitle.textContent = titulo;
-        modalBody.innerHTML = '<p>' + mensaje + '</p>';
+    modalTitle.textContent = titulo;
+    modalBody.innerHTML = '<p>' + mensaje + '</p>';
 
-        let modal = new bootstrap.Modal(document.getElementById('mensajeModal'));
-        modal.show();
+    let modal = new bootstrap.Modal(document.getElementById('mensajeModal'));
+    modal.show();
+}
+
+// Evento para validar el formulario al enviar
+document.getElementById('rucForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let rucInput = document.getElementById('numeroIdentificacion').value.trim();
+
+    if (rucInput === '') {
+        mostrarMensajeModal('Campo requerido', 'Por favor, ingresa tu número de identificación (RUC).');
+        return;
     }
 
-    // Evento para validar el formulario al enviar
-    document.getElementById('rucForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        let rucInput = document.getElementById('numeroIdentificacion').value.trim();
+    if (!(/^\d+$/.test(rucInput))) {
+        mostrarMensajeModal('Formato Incorrecto', 'El número de identificación debe contener solo números.');
+        return;
+    }
 
-        if (rucInput === '') {
-            mostrarMensajeModal('Campo requerido', 'Por favor, ingresa tu número de identificación (RUC).');
-            return;
-        }
+    if (!validarRuc(rucInput)) {
+        mostrarMensajeModal('RUC Incorrecto', 'El número de identificación (RUC) ingresado es inválido.');
+        return;
+    }
 
-        if (!(/^\d+$/.test(rucInput))) {
-            mostrarMensajeModal('Formato Incorrecto', 'El número de identificación debe contener solo números.');
-            return;
-        }
+    // Aquí puedes enviar el formulario si todo está correcto
+    this.submit();
+});
 
-        if (!validarRuc(rucInput)) {
-            mostrarMensajeModal('RUC Incorrecto', 'El número de identificación (RUC) ingresado es inválido.');
-            return;
-        }
+// Evento para cerrar el modal
+/* document.getElementById('mensajeModal').addEventListener('hidden.bs.modal', function() {
+    document.getElementById('rucForm').reset();
+    document.getElementById('numeroIdentificacion').classList.remove('is-valid', 'is-invalid');
+}); */
 
-        // Aquí puedes enviar el formulario si todo está correcto
-        this.submit();
-    });
-
-    // Evento para cerrar el modal
-    /* document.getElementById('mensajeModal').addEventListener('hidden.bs.modal', function() {
-        document.getElementById('rucForm').reset();
-        document.getElementById('numeroIdentificacion').classList.remove('is-valid', 'is-invalid');
-    }); */
-
-    // Evento para limpiar clases de validación cuando se escribe en el campo
-    /* document.getElementById('numeroIdentificacion').addEventListener('input', function() {
-        this.classList.remove('is-valid', 'is-invalid');
-    }); */
-
+// Evento para limpiar clases de validación cuando se escribe en el campo
+/* document.getElementById('numeroIdentificacion').addEventListener('input', function() {
+    this.classList.remove('is-valid', 'is-invalid');
+}); */
 </script>
 @endpush
--- resources/views/pages/pedido/index.blade.php
+++ resources/views/pages/pedido/index.blade.php
@@ -3,22 +3,186 @@
 @section('content')
 
 
+<header class=" p-3 mb-2 border-bottom">
+    <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3">
+        <a class="navbar-brand" href="#">@include('components.atoms.logo.zc_mayoristas')</a>
+        <ul class="nav nav-pills">
+            <li class="nav-item dropdown">
+                <img src="{{ asset('assets/imagenes/icono.png')}}" class="dropdown-toggle icono"
+                    data-bs-toggle="dropdown" alt="Dropdown" role="button" aria-expanded="false">
+                <ul class="dropdown-menu"
+                    style="position: absolute; inset: 0px 0px auto auto; margin: 0px; transform: translate(0px, 34px);">
+                    <li><a class="dropdown-item" href="#scrollspyHeading3">Third</a></li>
+                    <li><a class="dropdown-item" href="#scrollspyHeading4">Fourth</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#scrollspyHeading5">Cerrar sesión</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</header>
 <section>
-    <div class="container">
-        <div class="row flex-center min-vh-100 py-5">
-        <div class="container">
-    <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
+    <div class="container ">
+        <div class="row flex min-vh-100 py-5 ">
+            <div class="container text-center ">
+                <div class="row ">
+                    <div class="card shadow p-3 mb-5 bg-body-tertiary rounded" style="width: 18rem;">
+                        <img src="{{ asset('assets/imagenes/PDF_file_icon.svg.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Fecha: </li>
+                                <li class="list-group-item">Num. Pedido:</li>
+                                <li class="list-group-item">Valor a Pagar</li>
+                            </ul>
+                        </div>
+                        <div class="container text-center mx-auto">
+                            <button id="infoButton" class="btn btn-primary" type="button">información</button>
+                            <button id="pagarButton" class="btn btn-primary" type="button">Pagar</button>
+                        </div>
+                    </div>
+                    <div class="col">
+                        Column
+                    </div>
+                    <div class="col">
+                        Column
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
+<!-- Modal 1-->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="row g-3">
+                    <div class="col-md-6 border-bottom">
+                        <label for="inputEmail4" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="inputEmail4">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputPassword4" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4">
+                    </div>
+                    <div class="col-12">
+                        <label for="inputAddress" class="form-label">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                    </div>
+                    <div class="col-12">
+                        <label for="inputAddress2" class="form-label">Address 2</label>
+                        <input type="text" class="form-control" id="inputAddress2"
+                            placeholder="Apartment, studio, or floor">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="inputCity" class="form-label">City</label>
+                        <input type="text" class="form-control" id="inputCity">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="inputState" class="form-label">State</label>
+                        <select id="inputState" class="form-select">
+                            <option selected>Choose...</option>
+                            <option>...</option>
+                        </select>
+                    </div>
+                    <div class="col-md-2">
+                        <label for="inputZip" class="form-label">Zip</label>
+                        <input type="text" class="form-control" id="inputZip">
+                    </div>
+                    <div class="col-12">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="gridCheck">
+                            <label class="form-check-label" for="gridCheck">
+                                Check me out
+                            </label>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal 2 -->
+
+<div class="modal" id="pagarModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            
+                <h5 class="modal-title">Método de pago</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="container text-center">
+                <div class="d-flex flex-column flex-md-row p-4 gap-4 py-md-5 align-items-center justify-content-center">
+                    <div class="list-group list-group-radio d-grid gap-2 border-0">
+                    <img src="{{ asset('assets/imagenes/descarga.png')}}" style="width: 400px; height: auto;">
+
+                        <!-- <p class="chakra-text react-shadow-w0axkt">Método de pago</p> -->
+                        <div class="position-relative">
+                            <input class="form-check-input position-absolute top-50 end-0 me-3 fs-5" type="radio"
+                                name="listGroupRadioGrid" id="listGroupRadioGrid1" value="" checked="">
+                            <label class="list-group-item py-3 pe-5" for="listGroupRadioGrid1">
+                            <strong class="fw-semibold">
+                            <i class="bi bi-credit-card" style="color: black;"></i> Payphone</strong>
+                                <span class="d-block small opacity-75">With support text underneath to add more
+                                    detail</span>
+                            </label>
+                        </div>
+                        <div class="position-relative">
+                            <input class="form-check-input position-absolute top-50 end-0 me-3 fs-5" type="radio"
+                                name="listGroupRadioGrid" id="listGroupRadioGrid2" value="">
+                            <label class="list-group-item py-3 pe-5" for="listGroupRadioGrid2">
+                                <strong class="fw-semibold">
+                                <i class="bi bi-credit-card" style="color: black;"></i> Datafast</strong>
+                                <span class="d-block small opacity-75">Some other text goes here</span>
+                            </label>
+                        </div>
+
+                        <div class="position-relative">
+                            <input class="form-check-input position-absolute top-50 end-0 me-3 fs-5" type="radio"
+                                name="listGroupRadioGrid" id="listGroupRadioGrid3" value="">
+                            <label class="list-group-item py-3 pe-5" for="listGroupRadioGrid3">
+                                <strong class="fw-semibold">Third radio</strong>
+                                <span class="d-block small opacity-75">And we end with another snippet of text</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 @endsection
 @push('script-app')
+
 <script src="{{asset('assets/js/helper.js')}}"></script>
 <script>
-    
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('infoButton').addEventListener('click', function() {
+        var modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+        modal.show();
+    });
+
+    document.getElementById('pagarButton').addEventListener('click', function() {
+        var modal = new bootstrap.Modal(document.getElementById('pagarModal'));
+        modal.show();
+    });
+});
 </script>
 @endpush
