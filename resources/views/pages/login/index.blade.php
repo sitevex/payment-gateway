406814ef8bf59b7c7945f0d75a097eec8602f685
Miguel <tecnologia1@zcmayoristas.com>
2024-03-22 14:46:08 -0500

    Ajuste de commit

--- resources/views/pages/login/index.blade.php
+++ resources/views/pages/login/index.blade.php
@@ -35,7 +35,7 @@
                             @csrf
                             <div class="col-md-12">
                                 <label for="numeroIdentificacion" class="form-label fs-sm d-none">Número de idetificación</label>
-                                <input type="number" class="form-control form-control-lg" name="numeroIdentificacion" id="numeroIdentificacion" placeholder="Ingresa tu número de identificación (ruc)" required />
+                                <input type="number" class="form-control form-control-lg" name="numeroIdentificacion" id="numeroIdentificacion" placeholder="Ingresa tu número de identificación (ruc)" required data-error="Número de identificación incorrecto. Por favor, inténtalo de nuevo." />
                                 <div class="invalid-feedback">
                                     Número de identificación incorrecto. Por favor, inténtalo de nuevo.
                                 </div>
@@ -71,6 +71,80 @@
 @push('script-app')
 <script src="{{asset('assets/js/helper.js')}}"></script>
 <script>
+    let isValid = true;
+    let errorMessage = "";
+
+    // Evento para validar el formulario al enviar
+    const form = document.getElementById('rucForm');
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault();
+        let rucInput = document.getElementById('numeroIdentificacion').value.trim();
+        let isValid = true;
+        let errorMessage = "";
+
+        if (rucInput === '') {
+            mostrarMensajeModal('Campo requerido', 'Por favor, ingresa tu número de identificación (RUC).');
+            isValid = false;
+            errorMessage = 'Por favor, ingresa tu número de identificación (RUC).';
+            // return;
+        }
+
+        else if (!(/^\d+$/.test(rucInput))) {
+            mostrarMensajeModal('Formato Incorrecto', 'El número de identificación debe contener solo números.');
+            isValid =  false;
+            errorMessage = 'El número de identificación debe contener solo números';
+            // return;
+        }
+
+        else if (!validarRuc(rucInput)) {
+            mostrarMensajeModal('RUC Incorrecto', 'El número de identificación (RUC) ingresado es inválido.');
+            isValid = false;
+            errorMessage = 'El número de identificación (RUC) ingresado es inválido.';
+            // return;
+        }
+
+        if (!isValid) {
+            let errorDiv = document.getElementById('numeroIdentificacion').closest('.col-md-12').querySelector('.invalid-feedback');
+            errorDiv.textContent = errorMessage;
+            errorDiv.style.display = 'block';
+            return;
+        }
+
+        // Aquí puedes enviar el formulario si todo está correcto
+        const url = `/index?ruc=${rucInput}`;
+
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log(data); // Aquí puedes hacer algo con los datos, como mostrarlos en la página
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+        
+    });
+
+    // Evento para cerrar el modal
+    /* document.getElementById('mensajeModal').addEventListener('hidden.bs.modal', function() {
+        document.getElementById('rucForm').reset();
+        document.getElementById('numeroIdentificacion').classList.remove('is-valid', 'is-invalid');
+    }); */
+
+    // Evento para limpiar clases de validación cuando se escribe en el campo
+    /* document.getElementById('numeroIdentificacion').addEventListener('input', function() {
+        this.classList.remove('is-valid', 'is-invalid');
+    }); */
+
+    // Función para ocultar el mensaje de error al corregir el campo
+    /* document.getElementById('numeroIdentificacion').addEventListener('input', function() {
+        let errorDiv = this.closest('.col-md-12').querySelector('.invalid-feedback');
+        errorDiv.textContent = 'Por favor, ingresa tu número de identificación (RUC).';
+        errorDiv.style.display = 'none';
+    }); */
+
     // Función para mostrar el modal con el mensaje
     function mostrarMensajeModal(titulo, mensaje) {
         let modalTitle = document.querySelector('#mensajeModal .modal-title');
@@ -82,41 +156,6 @@
         let modal = new bootstrap.Modal(document.getElementById('mensajeModal'));
         modal.show();
     }
-
-    // Evento para validar el formulario al enviar
-    document.getElementById('rucForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-        let rucInput = document.getElementById('numeroIdentificacion').value.trim();
-
-        if (rucInput === '') {
-            mostrarMensajeModal('Campo requerido', 'Por favor, ingresa tu número de identificación (RUC).');
-            return;
-        }
-
-        if (!(/^\d+$/.test(rucInput))) {
-            mostrarMensajeModal('Formato Incorrecto', 'El número de identificación debe contener solo números.');
-            return;
-        }
-
-        if (!validarRuc(rucInput)) {
-            mostrarMensajeModal('RUC Incorrecto', 'El número de identificación (RUC) ingresado es inválido.');
-            return;
-        }
-
-        // Aquí puedes enviar el formulario si todo está correcto
-        this.submit();
-    });
-
-// Evento para cerrar el modal
-/* document.getElementById('mensajeModal').addEventListener('hidden.bs.modal', function() {
-    document.getElementById('rucForm').reset();
-    document.getElementById('numeroIdentificacion').classList.remove('is-valid', 'is-invalid');
-}); */
-
-    // Evento para limpiar clases de validación cuando se escribe en el campo
-    /* document.getElementById('numeroIdentificacion').addEventListener('input', function() {
-        this.classList.remove('is-valid', 'is-invalid');
-    }); */
 
 </script>
 @endpush
