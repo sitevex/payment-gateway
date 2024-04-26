2fcc25b518f7ca23e883a5c85bd52a8db2538371
Miguel <tecnologia1@zcmayoristas.com>
2024-04-26 12:29:23 -0500

    protected campos pag_dig_respuesta

--- app/Models/PasarelaPago.php
+++ app/Models/PasarelaPago.php
@@ -15,5 +15,41 @@ class PasarelaPago extends Model
      */
     protected $table = 'pag_dig_Respuesta';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'cardType',
+        'bin',
+        'lastDigits',
+        'deferredCode',
+        'deferred',
+        'cardBrandCode',
+        'cardBrand',
+        'amount',
+        'clientTransactionId',
+        'phoneNumber',
+        'statusCode',
+        'transactionStatus',
+        'messageCode',
+        'transactionId',
+        'document',
+        'currency',
+        'optionalParameter1',
+        'optionalParameter2',
+        'optionalParameter3',
+        'optionalParameter4',
+        'storeName',
+        'date',
+        'regionIso',
+        'transactionType',
+        'reference',
+        'payphone',
+    ];
+
     public $timestamps = false;
 }
